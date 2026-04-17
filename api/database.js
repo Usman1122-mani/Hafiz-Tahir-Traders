22ad24aaca18ff4fcc558062e4045fa45dfc0d41
Usman5676 <usmanriaz8to1@gmail.com>
2026-04-18 02:11:15 +0500

    chnage

--- api/database.js
+++ api/database.js
@@ -1,20 +1,12 @@
 const mysql = require("mysql2");
 
 const dbConfig = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT || 3306
 };
-
-// Only use SSL if connecting to a remote database host (like Aiven/AWS)
-if (process.env.DB_HOST && process.env.DB_HOST !== "localhost") {
-  dbConfig.ssl = {
-    minVersion: 'TLSv1.2',
-    rejectUnauthorized: true
-  };
-}
 
 const db = mysql.createConnection(dbConfig);
 
